a2489572095dfc9fd9531532e5d00f21b9417357
guolq <guolq01@yealy.com.cn>
2020-12-30 14:01:38 +0800

    美团地址修改+分享title设置

--- miniprogram/pages/basic/redpacket/redpacket.js
+++ miniprogram/pages/basic/redpacket/redpacket.js
@@ -17,7 +17,7 @@ Page({
       { 
         title: '美团56元外卖红包天天拿', 
         app_id:'wxde8ac0a21135c07d', 
-        app_path:'/index/pages/h5/h5', 
+        app_path:'/index/pages/h5/h5?weburl=https%3A%2F%2Frunion.meituan.com%2Furl%3Fkey%3D591ec05930c57331c1212b936e6785c1%26url%3Dhttps%253A%252F%252Fi.meituan.com%252Fawp%252Fhfe%252Fblock%252Fa13b87919a9ace9cfab4%252F89400%252Findex.html%253Fappkey%253D591ec05930c57331c1212b936e6785c1%253A000001%26sid%3D000001&lch=cps:waimai:5:591ec05930c57331c1212b936e6785c1:000001&f_token=1&f_userId=1', 
         color:"cyan", 
         image:"http://toolfk.xiuxiandou.com/yhq/coupe2/mt_hb.png", 
       }, 
@@ -96,6 +96,8 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return {
+      title: "美团饿了么大额红包，每日可领！"
+    };
   }
 })
